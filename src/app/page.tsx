334fe92828db4ef18f91a1f0c424cd0c3385be67
MarcelOlsen <marcelolsen26@gmail.com>
2024-12-02 22:35:30 +0100

    Set up TipTap editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <p>Home</p>
-      <Button>Click me</Button>
+      <Link href="/documents/123">Quick link</Link>
     </div>
   );
 };
