8ac27377265c647d95ebd10d691b69d6606877bb
MarcelOlsen <marcelolsen26@gmail.com>
2024-12-07 21:05:46 +0100

    Added user activity to the editor

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -25,6 +25,8 @@ import {
 import { BsFilePdf } from "react-icons/bs";
 
 import { DocumentInput } from "./components/document-input";
+import { Avatars } from "./components/avatars";
+
 import {
   Menubar,
   MenubarContent,
@@ -260,6 +262,7 @@ export const Navbar = () => {
         </div>
       </div>
       <div className="flex gap-3 items-center pl-6">
+        <Avatars />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
